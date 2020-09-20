efc44885a35a1141a8f9becbc5e6a0758475390e
Eloise Schmitt <eschmitt@atixnet.fr>
2020-09-20 09:19:02 +0200

    ajout du formulaire d'ajout d'article

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -6,6 +6,9 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\Article;
 use App\Repository\ArticleRepository;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class BlogController extends AbstractController
 {
@@ -31,6 +34,39 @@ class BlogController extends AbstractController
             'title' => "Bienvenue !"
         ]);
     }
+
+    /**
+     * @Route("/blog/new", name="blog_create")
+     */
+    public function create() {
+        $article = new Article();
+
+        $form = $this->createFormBuilder($article)
+                    ->add('title', TextType::class, [
+                        'attr' => [
+                            'placeholder' => "Titre de l'article",
+                            'class' => 'form-control'
+                        ]
+                    ])
+                    ->add('content', TextareaType::class, [
+                        'attr' => [
+                            'placeholder' => "Contenu de l'article",
+                            'class' => 'form-control'
+                        ]
+                    ])
+                    ->add('image', TextType::class, [
+                        'attr' => [
+                            'placeholder' => "Image de l'article",
+                            'class' => 'form-control'
+                        ]
+                    ])
+                    ->getForm();
+
+        return $this->render('blog/create.html.twig', [
+            'formArticle' => $form->createView()
+        ]);
+    }
+
     /**
      * @Route("/blog/{id}", name="blog_show")
      */
@@ -39,6 +75,5 @@ class BlogController extends AbstractController
             'article' => $article
         ]);
     }
-
     
 }
