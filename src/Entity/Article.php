aec1504b511341dad5040fec4da33eb2a9f7f712
Eloise Schmitt <eschmitt@atixnet.fr>
2020-09-20 15:45:06 +0200

    validation des formulaires

--- src/Entity/Article.php
+++ src/Entity/Article.php
@@ -4,6 +4,7 @@ namespace App\Entity;
 
 use App\Repository\ArticleRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass=ArticleRepository::class)
@@ -19,6 +20,7 @@ class Article
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Assert\length(min=10, max=255)
      */
     private $title;
 
@@ -34,6 +36,7 @@ class Article
 
     /**
      * @ORM\Column(type="datetime")
+     * @Assert\Url()
      */
     private $createdAt;
 
